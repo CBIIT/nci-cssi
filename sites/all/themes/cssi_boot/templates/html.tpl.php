8016705839508ca8e34d4a808674fed82512b3f0
Steve Mossner <smossner@gmail.com>
2017-10-13 14:59:47 -0400

    Added GA
    Added Google Analytics

--- sites/all/themes/cssi_boot/templates/html.tpl.php
+++ sites/all/themes/cssi_boot/templates/html.tpl.php
@@ -47,6 +47,16 @@
 ?>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <head profile="<?php print $grddl_profile; ?>">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-7993733-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-7993733-5');
+</script>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php print $head; ?>
@@ -58,6 +68,7 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 <?php print $scripts; ?>
+
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 <div id="skip-link"> <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a> </div>
